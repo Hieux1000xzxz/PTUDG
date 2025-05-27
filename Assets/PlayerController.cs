6e4332d0910ce4103bdc76184f9d2b948873fcaa
hieususano <truongduchieux6qt@gmail.com>
2025-05-27 23:30:05 +0700

    update player: idle, move

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        // Nhận input
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput = moveInput.normalized;
+
+        // Lật sprite theo hướng
+        if (moveInput.x > 0)
+            spriteRenderer.flipX = false;
+        else if (moveInput.x < 0)
+            spriteRenderer.flipX = true;
+
+        // Cập nhật animation
+        bool isMoving = moveInput.sqrMagnitude > 0;
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    void FixedUpdate()
+    {
+        // Di chuyển mượt và xử lý va chạm đúng cách
+        Vector2 newPosition = rb.position + moveInput * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
+    }
+
+}
+
